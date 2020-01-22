769f5ae708be110967f1c8cb45f17e934f950193
ypablo <yfedora@gmail.com>
2020-01-22 17:59:55 +0000

    Bug fix

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import classes from "./ContactData.module.css"
 import axios from '../../../axios-orders'
 import Spinner from "../../../components/UI/Spinner/Spinner"
+import Button from '../../../components/UI/Button/Button';
 
 
 export default class ContactData extends Component {
-    state={
-        name:"",
-        email:"",
+    state = {
+        name: "",
+        email: "",
         address: {
-            street:"",
-            postCode:"",
+            street: "",
+            postCode: "",
             country: ""
         },
         loading: false
@@ -46,16 +47,16 @@ export default class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your name"/>
-                <input className={classes.Input} type="text" name="email" placeholder="Your email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Your street"/>
-                <input className={classes.Input} type="text" name="postCode" placeholder="Your post code"/>
-                <input className={classes.Input} type="text" name="country" placeholder="Your country"/>
-                <button btnType="Success">ORDER</button>
+                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+                <input className={classes.Input} type="text" name="email" placeholder="Your email" />
+                <input className={classes.Input} type="text" name="street" placeholder="Your street" />
+                <input className={classes.Input} type="text" name="postCode" placeholder="Your post code" />
+                <input className={classes.Input} type="text" name="country" placeholder="Your country" />
+                <Button btnType="Success">ORDER</Button>
             </form>
         )
         if (this.state.loading) {
-            form = <Spinner/>
+            form = <Spinner />
         }
         return (
             <div className={classes.ContactData}>
